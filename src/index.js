9a657a68858d3408992544ee7a94d69742a8c76a
Mykhailo Potemko <105733943+potemko@users.noreply.github.com>
2023-03-22 11:32:16 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
 import './index.css';
+import user from "./library/user.json"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+const Card = () => { return(
+ user.map((photo) =>{
+  return(
+  <div key={photo.id} className="card" style={{width: "18rem"}}>
+    <img src={photo.avatar} className="card-img-top card__img" alt={photo.username} />
+    <div className="card-body">
+      <p className="card-text name-text">{photo.username}</p>
+      <p className="name-text name-tag">#{photo.tag}</p>
+      <p className="text-location">{photo.location}</p>
+    </div>
+    <ul className = "stats-list">
+      <li className="stats-item">
+        <span>followers</span>
+        <span>{photo.stats.followers}</span>
+      </li>
+      <li className="stats-item"><span>views</span>
+        <span>{photo.stats.views}</span></li>
+      <li className="stats-item"><span>likes</span>
+        <span>{photo.stats.likes}</span></li>
+    </ul>
+  </div>
+  )
+}))}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Card />);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
